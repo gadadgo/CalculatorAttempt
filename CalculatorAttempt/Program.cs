a27cfaf496acfc9ee0358a9c738909fd7f0da6b0
gadadgo <kretovich3@gmail.com>
2022-10-12 23:21:07 +0200

    basic

--- CalculatorAttempt/Program.cs
+++ CalculatorAttempt/Program.cs
@@ -1,1 +1,15 @@
 ﻿
+
+using System.Transactions;
+
+var introduction = "Calculator app. Enter the first number";
+
+Console.WriteLine($"{introduction}");
+
+var input1 = Console.ReadLine();
+Console.WriteLine("Enter the second number");
+var input2 = Console.ReadLine();
+
+
+var result = (int.Parse(input1)) + (int.Parse(input2));
+Console.WriteLine($"{result}");
