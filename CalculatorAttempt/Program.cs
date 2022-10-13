9a0fa26f573a2c822894588a142283928ff2a430
gadadgo <kretovich3@gmail.com>
2022-10-13 10:21:13 +0200

    Basic calc

--- CalculatorAttempt/Program.cs
+++ CalculatorAttempt/Program.cs
@@ -3,7 +3,10 @@
 using System.Transactions;
 
 Console.WriteLine($"Calculator app. Enter the first number");
-var input1 = Console.ReadLine();
+double input1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the next number");
+double input2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine($@"Choose the operation
 A - Add
@@ -11,9 +14,27 @@ S - Subtract
 M - Multiply
 D - Divide");
 
-var operation = Console.ReadLine();
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result {input1} + {input2} = " + (input1 + input2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result {input1} - {input2} = " + (input1 - input2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result {input1} * {input2} = " + (input1 * input2));
+        break;
+    case "d":
+        while (input2 == 0){
+            Console.WriteLine("Enter a non 0 value");
+            input2 = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine($"Your result {input1} / {input2} = " + (input1 / input2));
+        break;
+} 
 
-Console.WriteLine("Enter the next number");
-var input2 = Console.ReadLine();
+
+
 
 
